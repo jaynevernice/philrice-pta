f97ee6cfe290db91cf84379b8d6ab10b651b162e
jaynevernice <jaynevernice.professional@gmail.com>
2024-02-22 10:14:57 +0800

    Fixed Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -85,6 +85,8 @@
                                 <input
                                     class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                                     type="password" name="password" placeholder="Password" />
+
+                                <a href="#" class="block text-sm text-gray-600 my-4 hover:text-gray-900 text-right">Forgot Password?</a>
                             
                                 {{-- <a href="{{ route('login') }}"> --}}
                                     <button
