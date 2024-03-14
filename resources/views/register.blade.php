f5060bd89893ce4fcd53ae216b219faab9f7a7d3
jaynevernice <jaynevernice.professional@gmail.com>
2024-03-14 14:58:06 +0800

    Fixed margin in step 3 of registration page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -177,10 +177,11 @@
                                 {{-- Step 3 --}}
                                 <div class="step">
                                     <h3 class="mb-4 text-lg font-medium leading-none text-gray-900 dark:text-white">Credentials</h3>
-                                    <div>
+                                    <div class="mb-2">
                                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                         <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••">
-                                    </div>                        <div>
+                                    </div>                        
+                                    <div class="mb-2">
                                         <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                                         <input type="password" name="confirm_password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••">
                                     </div>
